fc5f60a9c294c985c74a46945ef44185bf0d6d7f
GAWOO <ghost0970@naver.com>
2023-04-13 12:50:21 +0900

    Add T_trueman

---
+++ src/JAVA_2023_04_06/T_trueman.java
@@ -0,0 +1,17 @@
+package JAVA_2023_04_06;
+
+import java.util.Date;
+
+public class T_trueman {
+	public static void main(String[] args) {
+		Date date = new Date();
+		int hour = date.getHours();
+		int minute = date.getMinutes();
+
+		System.out.println("현재 시간은 " + hour + "시 " + minute + "분입니다.");
+		if(hour >= 4 && hour < 12) System.out.println("Good Morning");
+		else if(hour >= 12 && hour < 18) System.out.println("Good Afternoon");
+		else if(hour >= 18 && hour < 22) System.out.println("Good Evening");
+		else System.out.println("Good Night");
+	}
+}
